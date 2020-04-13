fd4288c8477a73ba8d45eeecb0a47aeaaa6c779a
Ahasan Habib <shobuz.cse03@gmail.com>
2020-04-13 19:55:22 +0600

    update some config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -6,14 +6,14 @@ const development = {
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
-  dialect: 'mysql'
+  dialect: process.env.DIALECT
 }
 const test = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.TEST_DB_NAME,
   host: process.env.DB_HOST,
-  dialect: 'mysql'
+  dialect: process.env.DIALECT
 }
 
 const production = {
@@ -21,7 +21,7 @@ const production = {
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
-  dialect: 'mysql'
+  dialect: process.env.DIALECT
 }
 
 module.exports = {
